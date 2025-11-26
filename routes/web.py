d11d12a2bb9e42b347b843f109a1c449cb1c0cb6
Dian Wicaksono <99239495+DianKartiko@users.noreply.github.com>
2025-11-26 10:36:45 +0700

    Add comments for blueprint initiation and route

--- routes/web.py
+++ routes/web.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
 
+# --- Blueprint Initiation ---
 bp = Blueprint("route", __name__)
 
-
+# --- Blurprint Route ---
 @bp.route("/")
 def index():
     return render_template("index.html")
